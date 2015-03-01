c8dc918d76eaf60fab71fdeccd6513708dbffce0
Darrick J. Wong <djwong@larch.djwong.org>
2015-03-01 12:14:17 -0800

    gui: More tweaks to HTML output.

--- fmgui.py
+++ fmgui.py
@@ -1699,7 +1699,7 @@ class fmgui(QtGui.QMainWindow):
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-<title>%s(%s) on %s</title>
+<title>%s (%s) on %s</title>
 <style type="text/css">
 #overview {
 	font-family: monospace;
